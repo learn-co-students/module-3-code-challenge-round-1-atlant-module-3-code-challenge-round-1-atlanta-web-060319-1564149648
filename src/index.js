6f5f91b53154e4c28adf68e3097e44031b479f9e
github username <github email address>
2019-07-26 11:28:29 -0400

    CRUSHED IT

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+document.addEventListener('DOMContentLoaded', function() {
+
+  const imageId = 2986 //Enter your assigned imageId here
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
@@ -9,4 +9,59 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
+  const imageContainer = document.getElementById('image')
+  const nameContainer = document.getElementById('name')
+  const likesCount = document.getElementById('likes')
+  const likeButton = document.getElementById('like_button')
+  const commentForm = document.getElementById('comment_form')
+  const comment = document.getElementById('comment_input')
+  const commentContainer = document.getElementById('comments')
+
+  fetch(imageURL)
+  .then(resp => resp.json())
+  .then((image) => {
+    console.log(image);
+    imageContainer.src = image.url
+    nameContainer.innerText = image.name
+    likesCount.innerText = image.like_count
+    image.comments.forEach((comment) => {
+      commentLi = document.createElement('li')
+      commentLi.innerText = comment.content
+      commentContainer.append(commentLi)
+    })
+  })
+
+
+  likeButton.addEventListener("click", () => {
+    likesCount.innerText = parseInt(likesCount.innerText) + 1
+    fetch(likeURL, {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        image_id: imageId
+      })
+    })
+  })
+
+  commentForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+    commentLi = document.createElement('li')
+    commentLi.innerText = comment.value
+    commentContainer.append(commentLi)
+    fetch(commentsURL, {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        image_id: imageId,
+        content: comment.value
+      })
+    })
+
+  })
 })
